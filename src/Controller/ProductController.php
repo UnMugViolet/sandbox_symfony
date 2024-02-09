58d11009bf8dffc057a9fe85e11f99a9b51011f9
Paul Jaguin <paul@screenup.com>
2024-02-09 17:28:25 +0100

    Instanciate form for adding a product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 #[Route('/produits', name: 'product_')]
 class ProductController extends AbstractController
@@ -20,5 +24,26 @@ class ProductController extends AbstractController
     public function details(Product $product): Response
     {
         return $this->render('product/details.html.twig', compact('product'));
+    }
+
+    #[Route('ajouter', name: 'add')]
+    public function add(): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_details', ['slug' => $product->getSlug()]);
+        }
+
+        return $this->render('product/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
